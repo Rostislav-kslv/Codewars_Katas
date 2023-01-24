3d4054626ebb1d6f5ec07a6b87382effb727c253
Rostislav <70579830+Rostislav-kslv@users.noreply.github.com>
2023-01-24 17:42:13 +0200

    Create solution.cpp

---
+++ 5kyu/RGB To Hex Conversion/solution.cpp
@@ -0,0 +1,21 @@
+#include <sstream>
+
+class RGBToHex
+{
+  public:
+  static std::string rgb(int r, int g, int b);
+};
+
+
+std::string RGBToHex::rgb(int r, int g, int b){
+  std::stringstream ss;
+  char c[16]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+  if(r > 255) r = 255;
+  if(b > 255) b = 255;
+  if(g > 255) g = 255;
+  if(r<0) r=0;
+  if(g<0) g=0;
+  if(b<0) b=0;
+  ss<<c[(int)r/16]<<c[r%16]<<c[(int)g/16]<<c[g%16]<<c[(int)b/16]<<c[b%16];
+  return ss.str();
+}
